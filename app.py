b01f66361874abdce28a64a1acaa815df79f26ea
koopaGian <100879633+koopagian99@users.noreply.github.com>
2024-12-23 21:17:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def process_image():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
